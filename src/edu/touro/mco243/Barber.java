b525615451327b3cf348b725347488a749214a76
aarontager <tageraharon@gmail.com>
2020-10-22 16:16:38 -0400

    Initial commit

---
+++ src/edu/touro/mco243/Barber.java
@@ -0,0 +1,7 @@
+package edu.touro.mco243;
+
+public class Barber extends Thread {
+	public void run() {
+		System.out.println("Test");
+	}
+}
---
+++ src/edu/touro/mco243/Main.java
@@ -0,0 +1,11 @@
+package edu.touro.mco243;
+
+public class Main {
+	public static void main(String[] args) {
+		Barber[] barbers = {
+				new Barber(),
+				new Barber()
+		};
+		barbers[0].start();
+	}
+}
